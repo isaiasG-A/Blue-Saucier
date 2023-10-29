bf66e0d1d1ba7a386c8e9737d216dd731574219a
isaiasG-A <isaiasgarciaariza@gmail.com>
2023-10-28 18:55:07 -0700

    register form and state have been implemented

--- blue saucier/src/components/Login&Register/Register.jsx
+++ blue saucier/src/components/Login&Register/Register.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { dataBase} from '../firebase/index';
+import { collection, addDoc } from "firebase/firestore"; 
 
 function Register() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
-    <div>Register</div>
+    <div>
+      <form action="">
+        <label>
+          First Name:
+          <input 
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input 
+            type="text" 
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            />
+        </label>
+        <label>
+          Email:
+          <input 
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input 
+            type='text'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
