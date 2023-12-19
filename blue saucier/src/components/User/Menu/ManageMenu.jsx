a93a7020aef84f1df11a62ef6b415c4ba8f75632
isaiasG-A <isaiasgarciaariza@gmail.com>
2023-12-19 09:40:10 -0800

    user is now able to delete menus and the change is reflected immediately on the screen

--- blue saucier/src/components/User/Menu/ManageMenu.jsx
+++ blue saucier/src/components/User/Menu/ManageMenu.jsx
@@ -1,28 +1,35 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 import { db } from '../../firebase';
 import { Link } from 'react-router-dom';
 
 
 function ManageMenu({ username }) {
   const [menus, setMenus] = useState([]);
+  const [menuId, setMenuId] = useState([]);
 
 useEffect(() => {
   async function getMenus() {
     const querySnapshot = await getDocs(collection(db, `${username} menus`));
     const menusData = querySnapshot.docs.map(doc => doc.data());
-    setMenus(menusData)
-
-    console.log(menus)
+    const menusIds = querySnapshot.docs.map(doc => doc.id);
+    setMenus(menusData);
+    setMenuId([...menusIds]);
   }
   getMenus()
-}, [username])
+}, [username, menus])
+
+async function deleteMenu(index) {
+  let menu = menuId[index];
+ await (deleteDoc(doc(db, `${username} menus`, menu)))
+}
 
   return (
     <div>
       <Link to='/mainMenu'>Main Menu</Link>
       <Link to='/createMenu'>Create Menu</Link>
+
       {menus.map((data, index) => (
        <div className='menuContainer'>
         <h2>Category</h2>
@@ -33,9 +40,8 @@ useEffect(() => {
         <p>{data.dishName}</p>   
         <h2>Price</h2> 
         <p>${data.pricing}</p>
-        <button>Delete</button>
+        <button onClick={() => deleteMenu(index)}>Delete</button>
        </div>
-        
       ))}
     </div>
   )
