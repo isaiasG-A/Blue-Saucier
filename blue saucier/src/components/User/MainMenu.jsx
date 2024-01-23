27663cd26e027631122c5de51ba2d9cc546f41f9
isaiasG-A <isaiasgarciaariza@gmail.com>
2024-01-22 17:18:11 -0800

    user menu has been styled

--- blue saucier/src/components/User/MainMenu.jsx
+++ blue saucier/src/components/User/MainMenu.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CalendarData from '../GoogleCalendar/CalendarData';
+import menuPlanning from '../../images/menuCreate.jpg';
+import orderingLists from '../../images/menuList.jpg';
+import googleCalendar from '../../images/googleCalendar.png';
 
 function MainMenu({name, token, signout }) {
 
   return (
-    <div>
-      <h1>Hello, {name}</h1>
-      <Link to='/menuPlanning'>Menu Planning</Link>
-      <Link to='/orderingListMenu'>Ordering Lists</Link>
-      <Link onClick={() => CalendarData()}>Calendar</Link>
-      <button onClick={signout}>Logout</button>
+    <div className='userMenuContainer'>
+      <button className='ui inverted button logoutBttn' onClick={signout}>Logout</button>
+      <div className='userMenu'>
+     <h1>Hello, {name}</h1>
+
+     <div className='userMenuOptions'>
+        <Link style={{ cursor: 'pointer'}} to='/menuPlanning'>
+          <div class="ui card">
+            <a class="image" href="#">
+              <img src={menuPlanning}/>
+            </a>
+            <div class="content">
+              <a class="header" href="#">Menu Planing</a>
+            </div>
+         </div>
+        </Link>
+        <Link style={{ cursor: 'pointer'}} to='/orderingListMenu'>
+          <div class="ui card">
+            <a class="image" href="#">
+            <img src={orderingLists}/>
+            </a>
+            <div class="content">
+              <a class="header" href="#">Ordering Lists</a>
+            </div>
+          </div>
+        </Link>
+        <Link style={{cursor: 'pointer'}} onClick={() => CalendarData()}>
+          <div class="ui card">
+            <a class="image" href="#">
+            <img src={googleCalendar}/>
+            </a>
+            <div class="content">
+              <a class="header" href="#">Google Calendar</a>
+            </div>
+          </div>
+        </Link>
+      </div>
+     </div>
     </div>
   )
 }
--- blue saucier/src/components/User/Menu/MenuPlanning.jsx
+++ blue saucier/src/components/User/Menu/MenuPlanning.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import menuImg from '../../../images/menu.jpg'
 
 function MenuPlanning() {
   return (
     <div>
-      <Link to='/createMenu'>Create Menu</Link>
+      <Link to='/createMenu'>
+        <div class="ui card">
+          <a class="image" href="#">
+            <img src={menuImg}/>
+          </a>
+          <div class="content">
+            <a class="header" href="#">Create Menu</a>
+          </div>
+        </div>
+      </Link>
       <Link to='/manageMenu'>Manage Menu</Link>
     </div>
   )
