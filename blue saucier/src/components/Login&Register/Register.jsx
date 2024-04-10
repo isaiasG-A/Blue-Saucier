568e1d61da9c2ea66e381c9db88cb132c0d64780
Isaias <126319430+isaiasG-A@users.noreply.github.com>
2024-04-09 20:26:30 -0700

    Update Register.jsx
    user cannot be seen in the console anymore

--- blue saucier/src/components/Login&Register/Register.jsx
+++ blue saucier/src/components/Login&Register/Register.jsx
@@ -25,7 +25,6 @@ function Register() {
         displayName: `${firstName} ${lastName}`
       })
       const user = userCredential.user;
-      console.log(user)
     }).catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
